013f262f06c8be1f0bb3ab4e89aedfde333a60d2
Lu Gan <gl8997@gmail.com>
2016-08-29 12:19:21 +0800

    Update to use touchableNativeFeedback.

--- index.js
+++ index.js
@@ -1,95 +1,78 @@
 'use strict';
 
 import React, {
-    Component
+  Component
 } from 'react';
 
 import {
-    StyleSheet,
-    View,
-    Text,
-    TouchableOpacity,
-    Keyboard,
-    Platform,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableNativeFeedback
 } from 'react-native';
 
-type State = {
-    keyboardUp: boolean,
-}
+
+import {Actions, ActionConst} from 'react-native-router-flux';
 
 class Tabs extends Component {
-    state: State = {};
-
-    onSelect(el){
-        if (el.props.onSelect) {
-            el.props.onSelect(el);
-        } else if (this.props.onSelect) {
-            this.props.onSelect(el);
-        }
+  onSelect(el){
+    if (el.props.onSelect) {
+      el.props.onSelect(el);
+    } else if (this.props.onSelect) {
+      this.props.onSelect(el);
     }
+  }
 
-    componentWillMount(){
-        if (Platform.OS==='android') {
-            Keyboard.addListener('keyboardDidShow', this.keyboardWillShow);
-            Keyboard.addListener('keyboardDidHide', this.keyboardWillHide);
+  render(){
+    const self = this;
+    let selected = this.props.selected
+    if (!selected){
+      React.Children.forEach(this.props.children.filter(c=>c), el=>{
+        if (!selected || el.props.initial){
+          selected = el.props.name || el.key;
         }
+      });
     }
-
-    keyboardWillShow = (e) => {
-        this.setState({ keyboardUp: true });
-    };
-
-    keyboardWillHide = (e) => {
-        this.setState({ keyboardUp: false });
-    };
-
-    render(){
-        const self = this;
-        let selected = this.props.selected
-        if (!selected){
-            React.Children.forEach(this.props.children.filter(c=>c), el=>{
-                if (!selected || el.props.initial){
-                    selected = el.props.name || el.key;
-                }
-            });
-        }
-        return (
-            <View style={[styles.tabbarView, this.props.style, this.state.keyboardUp && styles.hidden]}>
-                {React.Children.map(this.props.children.filter(c=>c),(el)=>
-                    <TouchableOpacity key={el.props.name+"touch"}
-                       style={[styles.iconView, this.props.iconStyle, (el.props.name || el.key) == selected ? this.props.selectedIconStyle || el.props.selectedIconStyle || {} : {} ]}
-                       onPress={()=>!self.props.locked && self.onSelect(el)}
-                       onLongPress={()=>self.onSelect(el)}
-                       activeOpacity={el.props.pressOpacity}>
-                         {selected == (el.props.name || el.key) ? React.cloneElement(el, {selected: true, style: [el.props.style, this.props.selectedStyle, el.props.selectedStyle]}) : el}
-                    </TouchableOpacity>
-                )}
-            </View>
-        );
-    }
+    return (
+      <View style={[styles.tabbarView, this.props.style]}>
+      {React.Children.map(this.props.children.filter(c=>c),(el)=>
+        <TouchableNativeFeedback
+          onPress={()=>!self.props.locked && self.onSelect(el)}
+          onLongPress={()=>self.onSelect(el)}
+          delayPressIn={0}
+          delayPressOut={0}
+          background={TouchableNativeFeedback.SelectableBackground()}>
+          <TouchableOpacity key={el.props.name+"touch"}
+          style={[styles.iconView, this.props.iconStyle, (el.props.name || el.key) == selected ? this.props.selectedIconStyle || el.props.selectedIconStyle || {} : {} ]}
+          activeOpacity={el.props.pressOpacity}>
+          {selected == (el.props.name || el.key) ? React.cloneElement(el, {selected: true, style: [el.props.style, this.props.selectedStyle, el.props.selectedStyle]}) : el}
+          </TouchableOpacity>
+      </TouchableNativeFeedback>
+      )}
+      </View>
+    );
+  }
 }
 var styles = StyleSheet.create({
-    tabbarView: {
-        position:'absolute',
-        bottom:0,
-        right:0,
-        left:0,
-        height:50,
-        opacity:1,
-        backgroundColor:'transparent',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    iconView: {
-        flex: 1,
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    hidden: {
-        height: 0,
-    },
+  tabbarView: {
+    position:'absolute',
+    bottom:0,
+    right:0,
+    left:0,
+    height:50,
+    opacity:1,
+    backgroundColor:'transparent',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  iconView: {
+    flex: 1,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
 
 module.exports = Tabs;
